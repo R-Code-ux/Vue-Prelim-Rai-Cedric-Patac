cc27192190ad650a1bd285e1aed7ba6a0c64d1bf
R-Code-ux <raicedric117@gmail.com>
2025-08-26 11:45:03 +0800

    Rai-This is new vue

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,11 @@
 </template>
 
 <script setup>
+// create a Vue project, install Node.js first. Then install
+//  Vue CLI with npm install -g @vue/cli. Create a project using 
+// vue create my-vue-app, go into the folder (cd my-vue-app), and run it with npm run
+//  serve. Open your browser at http://localhost:8080 to see your app.
+
 import Greeting from './components/Greeting.vue';
 import Student from './components/Student.vue';
 
